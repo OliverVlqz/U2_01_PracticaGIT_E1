ce655583acd19bfae0979573fdb860ba0f5413fe
OliverVlqz <oliveratom772@gmail.com>
2025-05-20 15:50:00 -0600

    first commit

---
+++ PracticaGIT/src/test/java/mx/edu/utez/practicagit/PracticaGitApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.edu.utez.practicagit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticaGitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
