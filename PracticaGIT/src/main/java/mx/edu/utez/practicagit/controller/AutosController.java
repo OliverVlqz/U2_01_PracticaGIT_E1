ef67b86d2bc4e09ef17c075d93d53f800c0d14b4
Jorge-Moranchel <20243ds@utez.edu.mx>
2025-05-20 16:35:06 -0600

    Subiendo cambios

---
+++ PracticaGIT/src/main/java/mx/edu/utez/practicagit/controller/AutosController.java
@@ -0,0 +1,4 @@
+package mx.edu.utez.practicagit.controller;
+
+public class AutosController {
+}
---
+++ PracticaGITBack/src/main/java/com/example/practicagitback/model/Proveedores.java
@@ -0,0 +1,16 @@
+package com.example.practicagitback.model;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "proveedores")
+
+public class Proveedores {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(lenght = 100)
+    private String nombre;
+
+}
