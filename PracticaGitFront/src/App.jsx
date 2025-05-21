3140cdeff9335f17c2f5c159ee158b9983c7d681
alfredohern <20223tn060@utez.edu.mx>
2025-05-20 22:16:57 -0600

    feat(car information): add car view

--- PracticaGitFront/src/App.jsx
+++ PracticaGitFront/src/App.jsx
@@ -1,14 +1,12 @@
 import "./App.css";
 import Sidebar from "./components/Sidebar";
 import Car from "./pages/Cars";
-
 function App() {
   return (
     <>
       <div className="flex min-h-screen bg-white">
         <Sidebar />
 
-        {/* Aquí va tu contenido, por ejemplo: */}
         <Car />
       </div>
     </>
